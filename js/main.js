a569c09651d087d4c38d4ac5968391c0355bf4ee
GonzaBenedetti <143021859+GonzaBenedetti@users.noreply.github.com>
2023-10-19 10:21:20 -0300

    Preentrega01 - 03

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 let temp;
 let opc;
+let x, y;
 const quest = () => { 
     opc = prompt(`Ingrese una opción para realizar la operacion que desea:
     1 - De Celsius a Fahrenheit.
     2 - De Fahrenheit a Celsius.
     ESC - Para salir. `);
     opc = opc.toLowerCase();
+}
+const datos = () => {
+    temp = Number(prompt(`Ingrese la temperatura en ${x}:`));
+    while(isNaN(temp)) {
+        alert(`El dato ingresado no es un numero. Vuelva a intentar`);
+        temp = Number(prompt(`Vuelva a ingresar la temperatura en ${x}:`));
+    }
 }
 const celsiusFahrenheit = celsius => (celsius * 1.8) + 32;
 const fahrenheitCelsius = fahrenheit => (fahrenheit - 32) / 1.8;
@@ -15,21 +23,17 @@ quest ();
 while (opc != `esc`){
     switch (opc) {
         case `1`:
-            temp = Number(prompt(`Ingrese la temperatura en °C:`));
-            while(isNaN(temp)) {
-                alert(`El dato ingresado no es un numero. Vuelva a intentar`);
-                temp = Number(prompt(`Vuelva a ingresar la temperatura en °C:`));
-            }
-            alert(`La conversion de ${temp}°C a °F es: ` + celsiusFahrenheit(temp).toFixed(1) + `°F`);
+            x = `°C`;
+            y = `°F`
+            datos ();
+            alert(`La conversion de ${temp}${x} a ${y} es: ` + celsiusFahrenheit(temp).toFixed(1) + y);
             break;
 
         case `2`:
-            temp = Number(prompt(`Ingrese la temperatura en °F:`));
-            while(isNaN(temp)) {
-                alert(`El dato ingresado no es un numero. Vuelva a intentar`);
-                temp = Number(prompt(`Vuelva a ingresar la temperatura en °F:`));
-            }
-            alert(`La conversion de ${temp}°F a °C es: ` + fahrenheitCelsius(temp).toFixed(1) + `°C`);
+            x = `°F`
+            y = `°C`;
+            datos ();
+            alert(`La conversion de ${temp}${x} a ${y} es: ` + fahrenheitCelsius(temp).toFixed(1) + y);
             break;
         
         default:
